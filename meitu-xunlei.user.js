a8c75ed2de13ac8c234aff6b1422aa3712be9019
HapLeo <hnnk@qq.com>
2016-12-09 10:52:41 +0800

    Update meitu-xunlei.user.js

---
+++ meitu-xunlei.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         meitu-xunlei
+// @version      0.0.1
+// @description  白金会员也可观看超级会员视频－http://meitu.xunlei.com/videolist.html
+// @match        *://*.xunlei.com/*
+// @include      *://*.xunlei.com/*
+// @author       lgh06
+// @run-at       document-start
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var $ = s => document.querySelector(s);
+    var aaa = function(){
+        if(window.location.href.indexOf('meitu.xunlei.com')>=0){
+            if($('#vediocon')) $('#vediocon').style.display = 'none';
+            console.log(8888);
+        }
+        // Your code here...
+    };
+    setTimeout(aaa,5000);
+    })();
