b859df39a87056ac936153341ef22de94b99383d
HapLeo <hnnk@qq.com>
2017-06-14 11:40:47 +0800

    Create youdao-dict-scroll.user.js
    有道词典 自动滚动到权威词典部分

---
+++ youdao-dict-scroll.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         youdao-dict-scroll 有道词典 自动滚动到权威词典部分
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        http://dict.youdao.com/search?q=*
+// @run-at       document-idle
+// @grant        GM_xmlhttpRequest
+// @require      https://lib.sinaapp.com/js/jquery/2.2.4/jquery-2.2.4.min.js
+// ==/UserScript==
+(function (){
+  'use strict';
+  let e = document.querySelector('#authTrans h3 a span');
+  let rect = e.getBoundingClientRect();
+  let top = rect.top - 91; // 91为header的高度 暂时写死
+  jQuery('html,body').animate({
+    scrollTop: top + 'px'
+  },300);
+})();
