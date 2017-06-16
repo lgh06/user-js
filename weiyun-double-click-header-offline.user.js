c7113c9061e7955f76ba5db1c50220afd33dd2de
HapLeo <hnnk@qq.com>
2017-06-16 12:07:33 +0800

    Create weiyun-double-click-header-offline.user.js

---
+++ weiyun-double-click-header-offline.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         weiyun-add-offline-url
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  微云 双击标题栏 离线
+// @author       You
+// @match        https://www.weiyun.com/disk/index.html
+// @grant        none
+// @require      https://lib.sinaapp.com/js/jquery/2.2.4/jquery-2.2.4.min.js
+// @run-at       document-idle
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    let $ = jQuery.noConflict();
+    $('#_main_bar0').dblclick(()=>{
+      $('li[data-action="offline_download"]').click();
+      setTimeout(()=>$('li.link[data-tab="link"]').click(), 1000);
+    });
+
+})();
